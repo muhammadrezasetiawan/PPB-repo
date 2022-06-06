dd08dd8f8945b525be05c989da67c663a5adf9ea
muhammadrezasetiawan <72903655+muhammadrezasetiawan@users.noreply.github.com>
2022-06-06 20:42:34 +0700

    Tugas PPB

---
+++ Modul Praktikum 7/praktikum7/app/src/main/java/com/rezasetiawan_18102205/praktikum6/MyData.kt
@@ -0,0 +1,13 @@
+package com.rezasetiawan_18102205.praktikum6
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String,
+    val lat: Double,
+    val lang: Double
+) : Parcelable
