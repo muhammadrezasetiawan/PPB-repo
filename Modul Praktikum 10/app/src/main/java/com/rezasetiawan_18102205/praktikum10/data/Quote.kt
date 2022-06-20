1719047880e4df044a97bf4446d227672e9ab7b3
muhammadrezasetiawan <72903655+muhammadrezasetiawan@users.noreply.github.com>
2022-06-20 23:02:14 +0700

    Tugas PPB

---
+++ Modul Praktikum 10/app/src/main/java/com/rezasetiawan_18102205/praktikum10/data/Quote.kt
@@ -0,0 +1,13 @@
+package com.rezasetiawan_18102205.praktikum10.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
---
+++ Modul Praktikum 10/app/src/main/java/com/rezasetiawan_18102205/praktikum10/db/DatabaseContract.kt
@@ -0,0 +1,16 @@
+package com.rezasetiawan_18102205.praktikum10.db
+
+import android.provider.BaseColumns
+
+internal class DatabaseContract {
+    internal class QuoteColumns : BaseColumns {
+        companion object {
+            const val TABLE_QUOTE = "quote"
+            const val _ID = "_id"
+            const val TITLE = "title"
+            const val DESCRIPTION = "description"
+            const val CATEGORY = "category"
+            const val DATE = "date"
+        }
+    }
+}
